4af519e0cb19507fb6edf40b3ce984c3a0c4015a
Thanh Trong <ngthtrong0354@gmail.com>
2024-02-08 19:11:02 +0700

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')//yêu cầu thư viện express vừa cài để add vào chương trình
+const morgan = require('morgan')
 const app = express()//sử dụng toán tử call () để gọi express
 const port = 3000
 
-var a =1;
-var b =2;
-var c = a + b;
+
+app.use(morgan('combined'))
 
 //get ở đây chính là tạo ra router con đường dẫn tới web
 app.get('/', (req, res) => {
